743276a742ba28c4634c7944fb8e8468ec7e52a2
OP-Bright <rowanbrite@gmail.com>
2024-05-28 15:11:08 -0500

    Finished Matchey

--- data.js
+++ data.js
@@ -12,26 +12,52 @@
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+var animal = {};
 
+animal.species = "Komodo Dragon";
+
+animal["name"] = "Griz";
+
+animal.noises = [];
+
+console.log(animal);
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+var noises = [];
 
+noises[0] = "Hiss";
+
+noises.push("Chomp");
+
+noises.unshift("Gulp");
+
+noises[noises.length] = "More Hissing";
+
+console.log(noises.length)
+console.log(noises[noises.length - 1])
+console.log([noises])
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+animal["noises"] = noises;
 
+animal.noises.push("Chompy Chomp");
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
  *
+ * Rowan: brackets, and dot notation
+ * 
  * 2. What are the different ways of accessing elements on arrays?
+ * 
+ * Rowan: different methods, and also bracket notation
  *
  * *******************************************************************
  */
@@ -49,13 +75,57 @@
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+var animals = [];
 
+animals.push(animal);
+console.log(animals)
+
+var duck = {
+  species: "duck",
+  name: "Jerome",
+  noises: ["quack", "honk", "sneeze", "woosh"],
+}
+
+var crow = {
+  species: "crow",
+  name: "Artemis",
+  noises: ["caw", "coo"],
+}
+
+var lion = {
+  species: "lion",
+  name: "Leon",
+  noises: ["roar", "growl"],
+}
+
+animals.push(duck)
+animals.push(crow)
+animals.push(lion)
+console.log(animals)
+console.log(animals.length)
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//I choose an Array. The array can contain an array of objects,
+// so you can still access your friends and their properities.
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+var friends = [];
+
+function getRandom(animalsArray) {
+  return animalsArray[getRandomInt(animalsArray.length - 1)]
+}
+
+friends.push(getRandom(animals).name);
+
+var randomAnimal = getRandom(animals);
+
+randomAnimal["friends"] = friends;
 
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
--- functions.js
+++ functions.js
@@ -13,25 +13,58 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function search (animalArr, string) {
+    for (let i = 0; i < animalArr.length; i++) {
+        if (animalArr[i].name === string) {
+            return animalArr[i]
+        } 
+    }
+    return null
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function replace (animalArr, string, replacement) {
+    for (let i = 0; i < animalArr.length; i++) {
+        if (animalArr[i].name === string) {
+            animalArr[i] = replacement;
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function remove (animalArr, string) {
+    for (let i = 0; i < animalArr.length; i++) {
+        if (animalArr[i].name === string) {
+            animalArr.splice(i, 1);
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function add (animalArr, obj) {
+    let test = false;
+    if (obj.name.length > 0) {
+        if (obj.species.length > 0) {
+            for (let i = 0; i < animalArr.length; i++) {
+                if (animalArr[i].name === obj.name) {
+                    test = true
+                }
+            }
+            if (test === false) {
+                animalArr.push(obj)
+            }
+        }
+    }
+}
 
 /**
  * You did it! You're all done with Matchy!
